f49b625212306eb26207170099138f1ae99a6c00
Su.GG <suzanne.gonzalezgarcia@gmail.com>
2024-07-12 00:55:26 +0200

    updated documents

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 "use strict";
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2D");
+const canvas = document.querySelector("canvas"); // séléction du canvas
+const ctx = canvas.getContext("2d"); // déclaration du contexte
+
+// taille du canvas
+canvas.width = 690;
+canvas.height = 385;
+
+// variable score
+let showScore = window.document.getElementById("score");
+let score = 0;
+
+// charger les 6 skills dans un tableau
+const skills = [];
+const skillSrc = [
+  "img/html.png",
+  "img/css.png",
+  "img/js.png",
+  "img/jquery.png",
+  "img/bootstrap.png",
+  "img/ajax.png",
+  "img/git.png",
+];
+
+// définir une position différente de chaque image chargées
+const positions = [
+  { x: 20, y: 250 },
+  { x: 70, y: 150 },
+  { x: 150, y: 130 },
+  { x: 200, y: 230 },
+  { x: 270, y: 240 },
+  { x: 400, y: 200 },
+  { x: 550, y: 260 },
+];
+
+// charger chaque image et dessiner sur le canvas une fois chargée
+skillSrc.forEach((src, index) => {
+  const img = new Image();
+  img.src = src;
+  img.onload = function () {
+    const position = positions[index];
+    ctx.drawImage(img, position.x, position.y, 60, 60); //dessin de l'image aux coordonnées x et y
+  };
+  skills.push(img);
+});
 
 class MotionStatus {
   constructor() {
@@ -12,6 +54,8 @@ class MotionStatus {
     this.rightIntervalID = 0;
     this.bottom = false;
     this.bottomIntervalID = 0;
+    this.jump = false;
+    this.jumpIntervalID = 0;
   }
 }
 
@@ -25,6 +69,8 @@ class AnimationStatus {
     this.rightIntervalID = 0;
     this.bottom = false;
     this.bottomIntervalID = 0;
+    this.jump = false;
+    this.jumpIntervalID = 0;
   }
 }
 
@@ -49,44 +95,57 @@ class Interpolation {
   }
 }
 
+// class permettant le positionnement du sprite au mouvement en fonction de w,h,t,l
 class Game {
   constructor() {
     this.motionStatus = new MotionStatus();
     this.animationStatus = new AnimationStatus();
     this.spritePosition = {
       walking: [
-        new Interpolation("50px", "100px", "-920px", "-4px"),
-        new Interpolation("0px", "100px", "-920px", "-55px"),
-        new Interpolation("50px", "100px", "-920px", "-85px"),
-        new Interpolation("0px", "100px", "-920px", "-104px"),
-        new Interpolation("50px", "100px", "-920px", "-145px"),
-        new Interpolation("0px", "100px", "-920px", "-185px"),
-        new Interpolation("50px", "100px", "-920px", "-204px"),
-        new Interpolation("0px", "100px", "-920px", "-235px"),
+        new Interpolation("50px", "65px", "0px", "-20px"),
+        new Interpolation("50px", "65px", "0px", "-85px"),
+        new Interpolation("50px", "65px", "0px", "-150px"),
+        new Interpolation("50px", "65px", "0px", "-215px"),
+        new Interpolation("50px", "65px", "0px", "-280px"),
+        new Interpolation("50px", "65px", "0px", "-345px"),
+        new Interpolation("50px", "65px", "0px", "-410px"),
+        new Interpolation("50px", "65px", "0px", "-475px"),
+      ],
+      jumping: [
+        new Interpolation("50px", "65px", "-140px", "-20px"),
+        new Interpolation("50px", "65px", "-140px", "-85px"),
+        new Interpolation("50px", "65px", "-140px", "-150px"),
+        new Interpolation("50px", "65px", "-140px", "-215px"),
+        new Interpolation("50px", "65px", "-140px", "-280px"),
       ],
     };
   }
+  //positionnement de l'avatar
   initializeAvatar() {
-    this.avatar.style.top = "0px";
-    this.avatar.style.left = "0px";
+    this.avatar.style.top = "260px";
+    this.avatar.style.left = "350px";
     this.avatar.style.width = "60px";
-    this.avatar.style.height = "120px";
+    this.avatar.style.height = "65px";
     this.avatar.style.position = "absolute";
   }
 
+  //positionnement du masque
   initializeMask() {
+    this.masque.style.top = "0px";
+    this.masque.style.left = "0px";
     this.masque.style.width = "50px";
-    this.masque.style.height = "100px";
-    this.masque.style.overflow = "hidden"; // change to hidden
-    this.masque.style.position = "absolute";
-    this.masque.style.border = "1px solid pink"; // aide visu pour encadrer le sprite souhaité
+    this.masque.style.height = "65px";
+    this.masque.style.overflow = "hidden";
+    this.masque.style.position = "relative";
+    this.masque.style.border = "1px solid pink"; // aide visu pour encadrer le sprite souhaité - à mettre en commentaire pour la publication
   }
 
+  //positionnement du sprite
   initializeSprite() {
-    this.sprite.style.top = "-920px";
-    this.sprite.style.left = "-4px";
-    this.sprite.style.width = "1090px";
-    this.sprite.style.position = "relative";
+    this.sprite.style.top = "0px";
+    this.sprite.style.left = "-20px";
+    this.sprite.style.width = "600px";
+    this.sprite.style.position = "absolute";
   }
 
   move(direction) {
@@ -98,8 +157,11 @@ class Game {
     }
     this.avatar.style.left =
       parseFloat(this.avatar.style.left) + increment + "px";
+    this.checkCollisions();
+    this.redraw();
   }
 
+  //utilisation du même sprite effet miroir
   walkingAnimation(direction) {
     switch (direction) {
       case "left":
@@ -121,8 +183,100 @@ class Game {
       if (i < this.spritePosition.walking.length) {
         i++;
       }
-    }, 120);
+    }, 150);
     return walkingAnimationIdentifier;
+  }
+
+  jump() {
+    this.motionStatus.jumping = true;
+    const initialTop = parseFloat(this.avatar.style.top);
+    const maxHeight = 150; // Hauteur du saut
+    let up = true;
+    let height = 0;
+
+    const jumpIntervalID = window.setInterval(() => {
+      if (up) {
+        height += 5;
+        if (height >= maxHeight) up = false;
+      } else {
+        height -= 5;
+        if (height <= 0) {
+          height = 0;
+          this.motionStatus.jumping = false;
+          window.clearInterval(jumpIntervalID);
+        }
+      }
+      this.avatar.style.top = initialTop - height + "px";
+      //this.checkCollisions();
+      //this.redraw();
+    }, 25);
+
+    let i = 0;
+    const jumpAnimationID = window.setInterval(() => {
+      if (i >= this.spritePosition.jumping.length) {
+        i = 0;
+      }
+      this.masque.style.width = this.spritePosition.jumping[i].masque.width;
+      this.masque.style.height = this.spritePosition.jumping[i].masque.height;
+      this.sprite.style.left = this.spritePosition.jumping[i].sprite.left;
+      this.sprite.style.top = this.spritePosition.jumping[i].sprite.top;
+      if (i < this.spritePosition.jumping.length) {
+        i++;
+      }
+    }, 150);
+    return jumpAnimationID;
+  }
+
+  checkCollisions() {
+    const avatarRect = {
+      x: parseFloat(this.avatar.style.left),
+      y: parseFloat(this.avatar.style.top),
+      width: parseFloat(this.avatar.style.width),
+      height: parseFloat(this.avatar.style.height),
+    };
+
+    for (let i = 0; i < positions.length; i++) {
+      const skillRect = positions[i];
+
+      if (
+        avatarRect.x < skillRect.x + 400 + 60 && // 400 est nécessaire pour le bon positionnement de la pièce par rapport à l'avatar et 60 correspond à la width
+        avatarRect.x + avatarRect.width > skillRect.x + 400 &&
+        avatarRect.y < skillRect.y + 60 &&
+        avatarRect.y + avatarRect.height > skillRect.y
+      ) {
+        console.log("coucou i = " + i);
+
+        // Collision detectée, retrait de skill
+        positions.splice(i, 1); // Retrait de la position
+        skills.splice(i, 1); // Retrait de la skill touchée
+
+        // Incrementation du score
+        score++;
+        showScore.innerText = "SCORE: " + score;
+
+        // Score max de 7
+        if (score >= 7) {
+          alert(
+            "Félicitations ! Vous avez attraper toutes les compétences! Vous pouvez à présent télécharger mon CV"
+          );
+          document.getElementById("downloadCV").style.display = "block";
+        }
+
+        break; // Break after removing one skill to avoid issues with index changes
+      }
+    }
+  }
+
+  redraw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // efface le caneva
+    // Redessine les skills restantes
+    skillSrc.forEach((src, index) => {
+      if (index < positions.length) {
+        const img = skills[index];
+        const position = positions[index];
+        ctx.drawImage(img, position.x, position.y, 60, 60);
+      }
+    });
   }
 
   initialize() {
@@ -131,9 +285,9 @@ class Game {
       this.masque = window.document.querySelector("#masque");
       this.sprite = window.document.querySelector("#sprite");
 
+      this.initializeAvatar();
       this.initializeMask();
       this.initializeSprite();
-      this.initializeAvatar();
 
       window.addEventListener("keydown", (keyboardEvent) => {
         switch (keyboardEvent.key) {
@@ -163,6 +317,21 @@ class Game {
               }, 25);
             }
             break;
+          case " ":
+            if (!this.motionStatus.jumping) {
+              this.motionStatus.jump = true;
+              window.clearInterval(this.motionStatus.leftIntervalID);
+              this.motionStatus.left = false;
+              window.clearInterval(this.animationStatus.leftIntervalID);
+              window.clearInterval(this.motionStatus.leftIntervalID);
+              this.motionStatus.right = false;
+              window.clearInterval(this.animationStatus.rightIntervalID);
+              this.animationStatus.jumpIntervalID = this.jump("jump");
+              this.motionStatus.jumpIntervalID = window.setInterval(() => {
+                this.move("jump");
+              }, 25);
+            }
+            break;
         }
       });
       window.addEventListener("keyup", (keyboardEvent) => {
@@ -176,6 +345,11 @@ class Game {
             window.clearInterval(this.motionStatus.rightIntervalID);
             window.clearInterval(this.animationStatus.rightIntervalID);
             this.motionStatus.right = false;
+            break;
+          case " ":
+            window.clearInterval(this.motionStatus.jumpIntervalID);
+            window.clearInterval(this.animationStatus.jumpIntervalID);
+            this.motionStatus.jump = false;
             break;
         }
       });
